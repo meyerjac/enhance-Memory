758842ba6d25ff9fa3a1c776b312de2183990c0e
jackson meyer <jackson.meyer7@gmail.com>
2017-04-14 01:27:40 -0700

    initial UI

---
+++ app/src/main/java/jacksonmeyer/com/memoryenhancement/MainActivity.java
@@ -0,0 +1,19 @@
+package jacksonmeyer.com.memoryenhancement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
