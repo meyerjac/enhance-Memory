dd05032b98be0b2f62dad3f64f103d65392d358e
jackson meyer <jackson.meyer7@gmail.com>
2017-04-14 02:18:17 -0700

    imported fonts, bound views and got animation working

--- app/src/main/java/jacksonmeyer/com/memoryenhancement/MainActivity.java
+++ app/src/main/java/jacksonmeyer/com/memoryenhancement/MainActivity.java
@@ -1,19 +1,62 @@
 package jacksonmeyer.com.memoryenhancement;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.titleTextView)
+    TextView TitleTextView;
+    @Bind(R.id.noAdsButton)
+    TextView NoAdsButton;
+    @Bind(R.id.rateButton)
+    TextView RateButton;
+    @Bind(R.id.playButton)
+    TextView PlayButton;
+    @Bind(R.id. numberOfLightbulbs)
+    TextView NumberOfLightbulbs;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface Rubix = Typeface.createFromAsset(getAssets(), "fonts/Rubik-Regular.ttf");
+        TitleTextView.setTypeface(Rubix);
+        RateButton.setTypeface(Rubix);
+        PlayButton.setTypeface(Rubix);
+        NoAdsButton.setTypeface(Rubix);
+        NumberOfLightbulbs.setTypeface(Rubix);
 
         View decorView = getWindow().getDecorView();
 // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
+        final Handler handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Animation bounceAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bounce);
+                TitleTextView.startAnimation(bounceAnim);
+                TitleTextView.startAnimation(bounceAnim);
+                bounceAnim.setRepeatMode(Animation.REVERSE);
+            }
+        }, 0);
     }
 }
